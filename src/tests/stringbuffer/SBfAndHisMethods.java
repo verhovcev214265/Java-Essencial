d93b90637b5b53570f0fb67b3bd317a3be7b5075
Mark <verhovcev214265@gmail.com>
2021-11-01 17:56:12 +0200

    first commit to gitHub with new computer

--- src/tests/stringbuffer/SBfAndHisMethods.java
+++ src/tests/stringbuffer/SBfAndHisMethods.java
@@ -27,6 +27,7 @@ public class SBfAndHisMethods {
         naphanya.reverse();
         System.out.println(naphanya);
         System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("Hello Git");
 
     }
 }
