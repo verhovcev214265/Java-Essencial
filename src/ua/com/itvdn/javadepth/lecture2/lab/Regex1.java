fc38ee66cbc4a7ec34fb3b1c0b57ba36d1a326eb
vevdokimenko <evdokimenko@hotmail.com>
2022-08-03 18:55:41 +0300

    Check git

--- src/ua/com/itvdn/javadepth/lecture2/lab/Regex1.java
+++ src/ua/com/itvdn/javadepth/lecture2/lab/Regex1.java
@@ -1,6 +1,7 @@
 package ua.com.itvdn.javadepth.lecture2.lab;
 
-//Дана строка 'ahb acb aeb aeeb adcb axeb'. Напишите регулярное выражение, которая найдет строки ahb, acb, aeb по
+//Дана строка 'ahb acb aeb aeeb adcb axeb'.
+// Напишите регулярное выражение, которая найдет строки ahb, acb, aeb по
 //шаблону: буква 'a', любой символ, буква 'b'. Выведите строки на консоль
 
 import java.util.regex.Matcher;
